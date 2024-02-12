66a73ca525c4959e152aa906703e84ee51076e4d
Alexey Savchuk <alexey.a.savchuk@yandex.com>
2024-02-13 00:54:12 +0300

    fix(migrations): add missing column 'time' into table 'transfer'

--- migrations/002_create_transfer_table.up.sql
+++ migrations/002_create_transfer_table.up.sql
@@ -1,5 +1,6 @@
 CREATE TABLE transfer (
     transfer_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    time        TIMESTAMP DEFAULT now() NOT NULL,
     from_wallet UUID REFERENCES wallet(wallet_id) NOT NULL,
     to_wallet   UUID REFERENCES wallet(wallet_id) NOT NULL,
     amount      NUMERIC(15, 2) DEFAULT 100.00 NOT NULL
