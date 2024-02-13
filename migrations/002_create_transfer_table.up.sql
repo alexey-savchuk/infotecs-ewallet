6941508eeffc15c30b1f7c4a2707faf7ee58dd4c
Alexey Savchuk <alexey.a.savchuk@yandex.com>
2024-02-13 13:49:07 +0300

    feat(migrations): add 'amount > 0' check for 'transfer' table

--- migrations/002_create_transfer_table.up.sql
+++ migrations/002_create_transfer_table.up.sql
@@ -3,5 +3,5 @@ CREATE TABLE transfer (
     time        TIMESTAMP DEFAULT now() NOT NULL,
     from_wallet UUID REFERENCES wallet(wallet_id) NOT NULL,
     to_wallet   UUID REFERENCES wallet(wallet_id) NOT NULL,
-    amount      NUMERIC(15, 2) DEFAULT 100.00 NOT NULL
+    amount      NUMERIC(15, 2) DEFAULT 100.00 NOT NULL CHECK (amount > 0)
 );
