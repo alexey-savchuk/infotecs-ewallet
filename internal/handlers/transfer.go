851fbb926c39f49258794a6fdd2c6401107135fd
Alexey Savchuk <alexey.a.savchuk@yandex.com>
2024-02-14 18:04:57 +0300

    fix(handlers): change TransferDTO time field format to string

--- internal/handlers/transfer.go
+++ internal/handlers/transfer.go
@@ -1,13 +1,11 @@
 package handlers
 
 import (
-	"time"
-
 	"github.com/shopspring/decimal"
 )
 
 type TransferDTO struct {
-	Time       time.Time       `json:"time"`
+	Time       string          `json:"time"`
 	FromWallet string          `json:"from"`
 	ToWallet   string          `json:"to"`
 	Amount     decimal.Decimal `json:"amount"`
